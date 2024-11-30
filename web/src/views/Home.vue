6f4a740b8e4ffff3a9845ca92395a028b68b1e85
hh_zj <huangzhuji@huangzhujideMacBook-Air.local>
2024-11-30 17:13:38 +0800

    31.完成home前端

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-home
-
+    <div class="container">
+        <el-carousel height="calc(100vh - 62px)">
+            <el-carousel-item v-for="item in 1" :key="item">
+                <div :style="{backgroundImage: `url(${require('@/assets/home1.jpg')})`}"></div>
+            </el-carousel-item>
+            <el-carousel-item v-for="item in 2" :key="item">
+                <div :style="{backgroundImage: `url(${require('@/assets/home3.jpg')})`}"></div>
+            </el-carousel-item>
+        </el-carousel>
+        <div class="center">互联网+时代</div>
     </div>
 </template>
 
@@ -10,5 +17,25 @@ home
 </script>
 
 <style lang="scss" scoped>
+.el-carousel div{
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+}
 
+.container{
+    position: relative;
+}
+
+.center{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 100px;
+    line-height: 100px;
+    font-size: 60px;
+    color: white;
+    text-align: center;
+}
 </style>
